31140721b817028acfce5f2759f7c220300fffab
Cann-E <cann0211@gmail.com>
2026-01-27 22:46:53 -0600

    !

--- Arrays_Hashing/neetcode_questions/two_sum.py
+++ Arrays_Hashing/neetcode_questions/two_sum.py
@@ -3,9 +3,26 @@
 # You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
 
 
-class Solution:
+class Solution: #brute force n*2
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
+
+
+
+class Solution:#using hashmap 0(n)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        
+        for index,num in enumerate(nums):
+            diff = target - num
+            if diff in hashmap:
+                return [hashmap[diff] , index]
+            hashmap[num] = index
+            
+        return
+        
+
+        
